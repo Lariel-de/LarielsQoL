d8d92a6370987f845a79c545b717c2da3e71466a
Lariel <nadine.hoffmann1989@outlook.com>
2026-09-06 10:50:17 +0200

    Open Pokebag key now also checks the chest slot.

--- src/main/java/de/lariel/qualityoflife/utility/LarielPokeBagHelper.java
+++ src/main/java/de/lariel/qualityoflife/utility/LarielPokeBagHelper.java
@@ -3,18 +3,28 @@ package de.lariel.qualityoflife.utility;
 import com.pixelmonmod.pixelmon.items.PokeBagItem;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 
 public class LarielPokeBagHelper {
     public static void openFirstPokeBag(ServerPlayer player) {
+        ItemStack chestStack = player.getItemBySlot(EquipmentSlot.CHEST);
+        if (chestStack.getItem() instanceof PokeBagItem) {
+            openPokeBag(chestStack, player);
+            return;
+        }
+
         for (ItemStack stack : player.getInventory().items) {
             if (stack.getItem() instanceof PokeBagItem) {
-                PokeBagItem.openPokeBag(stack, player);
+                openPokeBag(stack, player);
                 return;
             }
         }
 
         player.sendSystemMessage(Component.translatable("key.larielsqualityoflife.no_poke_bag_found"));
     }
-}
 
+    private static void openPokeBag(ItemStack stack, ServerPlayer player) {
+        PokeBagItem.openPokeBag(stack, player);
+    }
+}
